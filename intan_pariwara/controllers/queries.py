6e2eb6b7220fff54d95631b774f376e9d1fbb39e
LAPTOP-FG7VS8C7\Malik <abdul.malik.ahmad997@gmail.com>
2025-01-02 09:38:21 +0000

    ref (price list): msih menggunakan 1 price list

--- intan_pariwara/controllers/queries.py
+++ intan_pariwara/controllers/queries.py
@@ -13,11 +13,11 @@ def get_price_list_fund(
 ):
     party = frappe.get_doc("Customer", customer)
     party_details = {
-        "selling_price_list": party.default_price_list or frappe.db.get_value("Selling Settings", None, "selling_price_list")
+        # "selling_price_list": party.default_price_list or frappe.db.get_value("Selling Settings", None, "selling_price_list")
     }
 
-    if fund_source and fund_source != party.custom_customer_fund_group:
-        party_details["selling_price_list"] = frappe.get_value("Fund Source Detail", {"parent": customer, "fund_source": fund_source}, "price_list") or party_details["selling_price_list"]
+    # if fund_source and fund_source != party.custom_customer_fund_group:
+    #     party_details["selling_price_list"] = frappe.get_value("Fund Source Detail", {"parent": customer, "fund_source": fund_source}, "price_list") or party_details["selling_price_list"]
 
     party_details["apply_rebate"] = frappe.get_value("Customer Fund Source", 
         party.get("custom_customer_fund_group"), "apply_rebate"
