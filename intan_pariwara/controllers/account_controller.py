d6d8efbfdb74c99003059bfc7334d8f7febfe511
LAPTOP-FG7VS8C7\Malik <abdul.malik.ahmad997@gmail.com>
2025-01-06 08:43:34 +0000

    ref (pre order): tambahkan validate payment schde

--- intan_pariwara/controllers/account_controller.py
+++ intan_pariwara/controllers/account_controller.py
@@ -2,6 +2,12 @@
 # License: GNU General Public License v3. See license.txt
 
 class AccountsController:
+    def validate_all_documents_schedule(self):
+        if self.doctype in ("Sales Invoice", "Purchase Invoice"):
+            self.validate_invoice_documents_schedule()
+        elif self.doctype in ("Pre Order","Quotation", "Purchase Order", "Sales Order"):
+            self.validate_non_invoice_documents_schedule()
+                     
     def calculate_taxes_and_totals(self):
         from intan_pariwara.controllers.taxes_and_totals import calculate_taxes_and_totals
 
