cedabb98e57f85f8740190cdb9cb38dbd775dce0
LAPTOP-FG7VS8C7\Malik <abdul.malik.ahmad997@gmail.com>
2025-02-26 05:07:44 +0000

    feat(report): sales person performance dan tambahin field rebate rate

---
+++ intan_pariwara/intan_pariwara/report/sales_person_performance/sales_person_performance.js
@@ -0,0 +1,32 @@
+// Copyright (c) 2025, DAS and contributors
+// For license information, please see license.txt
+
+frappe.query_reports["Sales Person Performance"] = {
+	"filters": [
+		{
+			fieldname: "sales_person",
+			label: __("Sales Person"),
+			fieldtype: "Link",
+			options: "Sales Person",
+		},
+		{
+			fieldname: "from_date",
+			label: __("From Date"),
+			fieldtype: "Date",
+			default: erpnext.utils.get_fiscal_year(frappe.datetime.get_today(), true)[1],
+		},
+		{
+			fieldname: "to_date",
+			label: __("To Date"),
+			fieldtype: "Date",
+			default: frappe.datetime.get_today(),
+		},
+		{
+			fieldname: "company",
+			label: __("Company"),
+			fieldtype: "Link",
+			options: "Company",
+			default: frappe.defaults.get_user_default("Company"),
+		},
+	]
+};
