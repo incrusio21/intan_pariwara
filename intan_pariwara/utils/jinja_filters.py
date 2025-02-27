b3b5cc4c3a06534e5d8b14d10b705aeb17eafdf1
incrusio21 <abdul.malik.ahmad997@gmail.com>
2025-02-27 06:21:12 +0000

    ref(jinja): tambahan error message jika nomor kosong

--- intan_pariwara/utils/jinja_filters.py
+++ intan_pariwara/utils/jinja_filters.py
@@ -1,7 +1,13 @@
 # Copyright (c) 2025, DAS and contributors
 # For license information, please see license.txt
 
+import frappe
+from frappe import _
+
 def format_nomor_telepon(nomor_telepon):
+    if not nomor_telepon:
+        frappe.throw(_("Please fill in your phone number."))
+        
     if nomor_telepon.startswith('0'):
         return '62' + nomor_telepon[1:]
     
