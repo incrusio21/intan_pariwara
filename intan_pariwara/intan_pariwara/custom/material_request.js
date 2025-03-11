547163e48e5dc7f38bba5b8fa598902106045962
LAPTOP-FG7VS8C7\Malik <abdul.malik.ahmad997@gmail.com>
2025-03-11 04:56:23 +0000

    feat(packing list): packing list bisa terbentuk dari material request transfer

---
+++ intan_pariwara/intan_pariwara/custom/material_request.js
@@ -0,0 +1,21 @@
+// Copyright (c) 2024, DAS and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on("Material Request", {
+    refresh: function (frm) {
+        if (frm.doc.docstatus == 1 && frm.doc.status != "Stopped" 
+            && frm.doc.material_request_type === "Material Transfer"
+            && flt(frm.doc.per_packing, precision) < 100) {
+                frm.add_custom_button(
+                    __("Packing List"),
+                    () => {
+                        frappe.model.open_mapped_doc({
+                            method: "intan_pariwara.intan_pariwara.custom.material_request.make_packing_list",
+                            frm: frm,
+                        });
+                    },
+                    __("Create")
+                );
+        }
+    }
+})
--- intan_pariwara/intan_pariwara/doctype/packing_list/packing_list.js
+++ intan_pariwara/intan_pariwara/doctype/packing_list/packing_list.js
@@ -3,7 +3,7 @@
 
 frappe.ui.form.on("Packing List", {
 	setup: (frm) => {
-		frm.set_query("wareshoue", () => {
+		frm.set_query("warehouse", () => {
 			return {
 				filters: {
 					is_group: 0,
@@ -11,24 +11,30 @@ frappe.ui.form.on("Packing List", {
 			};
 		});
 
-		frm.set_query("sales_order", () => {
+		frm.set_query("doc_name", (doc) => {
+			var filters = {
+				docstatus: 1,
+				per_packing: ["<", 100]
+			}
+
+			if(doc.purpose == "Material Request"){
+				filters["material_request_type"] = "Material Transfer"
+			}
+
 			return {
-				filters: {
-					docstatus: 1,
-					per_packing: ["<", 100]
-				},
+				filters: filters,
 			};
 		});
 
 		frm.set_query("item_code", "items", (doc, cdt, cdn) => {
-			if (!doc.sales_order) {
-				frappe.throw(__("Please select a Sales Order"));
+			if (!doc.doc_name) {
+				frappe.throw(__("Please select a " + doc.purpose));
 			} else {
 				let d = locals[cdt][cdn];
 				return {
 					query: "intan_pariwara.intan_pariwara.doctype.packing_list.packing_list.item_details",
 					filters: {
-						sales_order: doc.sales_order,
+						sales_order: doc.doc_name,
 					},
 				};
 			}
@@ -37,7 +43,7 @@ frappe.ui.form.on("Packing List", {
 
 	refresh: (frm) => {
 		if (frm.doc.docstatus == 1) {
-			if(frm.doc.per_delivered < 100){
+			if(frm.doc.purpose == "Sales Order" && frm.doc.per_delivered < 100){
 				frm.add_custom_button(
 					__("Delivery Note"),
 					() => {
@@ -51,20 +57,48 @@ frappe.ui.form.on("Packing List", {
 					__("Create")
 				);
 			}
+
+			if(frm.doc.purpose == "Material Request" && frm.doc.per_delivered < 100){
+				frm.add_custom_button(
+					__("Material Transfer"),
+					() => {
+						frappe.model.open_mapped_doc({
+							method: "intan_pariwara.intan_pariwara.doctype.packing_list.packing_list.make_stock_entry",
+							frm: frm,
+							freeze: true,
+							freeze_message: __("Creating Material Transfer ..."),
+						});
+					},
+					__("Create")
+				);
+			}
 		}
 	},
 
-	sales_order: (frm) => {
-		frm.set_value("items", null);
+	purpose: (frm) => {
+		frm.events.doc_name(frm)
+	},
 
-		if (frm.doc.sales_order) {
-			erpnext.utils.map_current_doc({
-				method: "intan_pariwara.intan_pariwara.custom.sales_order.make_packing",
-				source_name: frm.doc.sales_order,
-				target_doc: frm,
-				freeze: true,
-				freeze_message: __("Creating Packing List ..."),
-			});
+	doc_name: (frm) => {
+		frm.set_value("items", null);
+		
+		var purpose_method = {
+			"Material Request": "intan_pariwara.intan_pariwara.custom.material_request.make_packing_list",
+			"Sales Order": "intan_pariwara.intan_pariwara.custom.sales_order.make_packing_list",
 		}
+
+		if (frm.doc.doc_name) {
+			frm.events.get_items(frm, purpose_method[frm.doc.purpose])
+		}
+	},
+
+	get_items: (frm, method) => {
+		erpnext.utils.map_current_doc({
+			method: method,
+			source_name: frm.doc.doc_name,
+			target_doc: frm,
+			freeze: true,
+			freeze_message: __("Creating Packing List ..."),
+		});
 	},
 });
