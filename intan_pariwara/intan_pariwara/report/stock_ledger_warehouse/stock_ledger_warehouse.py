5e75bff78913043786c891434b5feab038001e2c
LAPTOP-FG7VS8C7\Malik <abdul.malik.ahmad997@gmail.com>
2025-05-08 07:34:09 +0000

    feat(report): kartu stock sesuai dengan permintaan ip

---
+++ intan_pariwara/intan_pariwara/report/stock_ledger_warehouse/stock_ledger_warehouse.py
@@ -0,0 +1,220 @@
+# Copyright (c) 2025, DAS and contributors
+# For license information, please see license.txt
+
+import copy
+from collections import defaultdict
+
+import frappe
+from frappe import _, scrub
+from frappe.query_builder.functions import CombineDatetime, Sum
+from frappe.utils import cint, flt, get_datetime
+
+from erpnext.stock.doctype.inventory_dimension.inventory_dimension import get_inventory_dimensions
+from erpnext.stock.doctype.serial_no.serial_no import get_serial_nos
+from erpnext.stock.doctype.stock_reconciliation.stock_reconciliation import get_stock_balance_for
+from erpnext.stock.doctype.warehouse.warehouse import apply_warehouse_filter
+from erpnext.stock.utils import (
+	is_reposting_item_valuation_in_progress,
+	update_included_uom_in_report,
+)
+from erpnext.stock.report.stock_ledger.stock_ledger import (
+	check_inventory_dimension_filters_applied,
+	get_item_details, 
+	get_items,
+	get_opening_balance, 
+	get_opening_balance_from_batch,
+	get_segregated_bundle_entries,
+	get_serial_batch_bundle_details, 
+	get_stock_ledger_entries,
+	update_available_serial_nos
+)
+
+def execute(filters=None):
+	is_reposting_item_valuation_in_progress()
+	include_uom = filters.get("include_uom")
+	columns = get_columns(filters)
+	items = get_items(filters)
+	sl_entries = get_stock_ledger_entries(filters, items)
+	item_details = get_item_details(items, sl_entries, include_uom)
+	if filters.get("batch_no"):
+		opening_row = get_opening_balance_from_batch(filters, columns, sl_entries)
+	else:
+		opening_row = get_opening_balance(filters, columns, sl_entries)
+		
+	precision = cint(frappe.db.get_single_value("System Settings", "float_precision"))
+	bundle_details = {}
+
+	if filters.get("segregate_serial_batch_bundle"):
+		bundle_details = get_serial_batch_bundle_details(sl_entries, filters)
+
+	data = []
+	conversion_factors = []
+	if opening_row:
+		data.append(opening_row)
+		conversion_factors.append(0)
+
+	actual_qty = stock_value = 0
+	if opening_row:
+		actual_qty = opening_row.get("qty_after_transaction")
+		stock_value = opening_row.get("stock_value")
+
+	available_serial_nos = {}
+	inventory_dimension_filters_applied = check_inventory_dimension_filters_applied(filters)
+
+	batch_balance_dict = frappe._dict({})
+	if actual_qty and filters.get("batch_no"):
+		batch_balance_dict[filters.batch_no] = [actual_qty, stock_value]
+
+	voucher_party = {}
+	for sle in sl_entries:
+		item_detail = item_details[sle.item_code]
+
+		sle.update(item_detail)
+		if bundle_info := bundle_details.get(sle.serial_and_batch_bundle):
+			data.extend(get_segregated_bundle_entries(sle, bundle_info, batch_balance_dict, filters))
+			continue
+
+		if filters.get("batch_no") or inventory_dimension_filters_applied:
+			actual_qty += flt(sle.actual_qty, precision)
+			stock_value += sle.stock_value_difference
+			if sle.batch_no:
+				if not batch_balance_dict.get(sle.batch_no):
+					batch_balance_dict[sle.batch_no] = [0, 0]
+
+				batch_balance_dict[sle.batch_no][0] += sle.actual_qty
+
+			if filters.get("segregate_serial_batch_bundle"):
+				actual_qty = batch_balance_dict[sle.batch_no][0]
+
+			if sle.voucher_type == "Stock Reconciliation" and not sle.actual_qty:
+				actual_qty = sle.qty_after_transaction
+				stock_value = sle.stock_value
+
+			sle.update({"qty_after_transaction": actual_qty, "stock_value": stock_value})
+
+		sle.update({"in_qty": max(sle.actual_qty, 0), "out_qty": min(sle.actual_qty, 0)})
+		
+		if sle.serial_no:
+			update_available_serial_nos(available_serial_nos, sle)
+		
+		if sle.actual_qty:
+			sle["in_out_rate"] = flt(sle.stock_value_difference / sle.actual_qty, precision)
+
+		elif sle.voucher_type == "Stock Reconciliation":
+			sle["in_out_rate"] = sle.valuation_rate
+
+		if not sle.voucher_type == "Stock Reconciliation":
+			sle["opening_qty"] = flt(sle["qty_after_transaction"] - sle.actual_qty)
+
+		if sle.voucher_type in ["Purchase Receipt", "Purchase Invoice", "Delivery Note", "Sales Invoice"]:
+			key = (sle.voucher_type, sle.voucher_no)
+			if key not in voucher_party:
+				party_type = "Customer" if sle.voucher_type in ["Delivery Note", "Sales Invoice"] else "Supplier"
+				party = frappe.get_value(sle.voucher_type, sle.voucher_no, scrub(party_type))
+				voucher_party.setdefault(key, {"party_type": party_type, "party": party})
+
+			sle.update(voucher_party.get(key, {}))
+
+		data.append(sle)
+
+		if include_uom:
+			conversion_factors.append(item_detail.conversion_factor)
+
+	update_included_uom_in_report(columns, data, include_uom, conversion_factors)
+	return columns, data
+
+def get_columns(filters):
+	columns = [
+		{"label": _("Date"), "fieldname": "date", "fieldtype": "Datetime", "width": 150},
+		{
+			"label": _("Item"),
+			"fieldname": "item_code",
+			"fieldtype": "Link",
+			"options": "Item",
+			"width": 100,
+		},
+		{"label": _("Item Name"), "fieldname": "item_name", "width": 100},
+		{
+			"label": _("Stock UOM"),
+			"fieldname": "stock_uom",
+			"fieldtype": "Link",
+			"options": "UOM",
+			"width": 90,
+		},
+	]
+
+	for dimension in get_inventory_dimensions():
+		columns.append(
+			{
+				"label": _(dimension.doctype),
+				"fieldname": dimension.fieldname,
+				"fieldtype": "Link",
+				"options": dimension.doctype,
+				"width": 110,
+			}
+		)
+
+	columns.extend(
+		[
+			{
+				"label": _("Opening Qty"),
+				"fieldname": "opening_qty",
+				"fieldtype": "Float",
+				"width": 80,
+				"convertible": "qty",
+			},
+			{
+				"label": _("In Qty"),
+				"fieldname": "in_qty",
+				"fieldtype": "Float",
+				"width": 80,
+				"convertible": "qty",
+			},
+			{
+				"label": _("Out Qty"),
+				"fieldname": "out_qty",
+				"fieldtype": "Float",
+				"width": 80,
+				"convertible": "qty",
+			},
+			{
+				"label": _("Balance Qty"),
+				"fieldname": "qty_after_transaction",
+				"fieldtype": "Float",
+				"width": 100,
+				"convertible": "qty",
+			},
+			{"label": _("Party Type"), "fieldname": "party_type", "width": 110, "hidden": 1},
+			{
+				"label": _("Party"),
+				"fieldname": "party",
+				"fieldtype": "Dynamic Link",
+				"width": 100,
+				"options": "party_type",
+			},
+			{"label": _("Transaction Type"), "fieldname": "voucher_type", "width": 110, "hidden": 1},
+			{
+				"label": _("Transaction No"),
+				"fieldname": "voucher_no",
+				"fieldtype": "Dynamic Link",
+				"options": "voucher_type",
+				"width": 100,
+			},
+			{
+				"label": _("Warehouse"),
+				"fieldname": "warehouse",
+				"fieldtype": "Link",
+				"options": "Warehouse",
+				"width": 150,
+			},
+			{
+				"label": _("Item Group"),
+				"fieldname": "item_group",
+				"fieldtype": "Link",
+				"options": "Item Group",
+				"width": 100,
+			},
+		]
+	)
+
+	return columns
